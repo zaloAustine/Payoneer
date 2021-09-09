6a8443b2b9fec8d9e49554052779cd395afdfa1b
austinezaloa@gmail.com <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>
2021-09-09 16:59:53 +0300

    rechecked test

--- app/src/test/java/com/zalocoders/payoneer/payment_options/viewmodel/PaymentOptionViewModelTest.java
+++ app/src/test/java/com/zalocoders/payoneer/payment_options/viewmodel/PaymentOptionViewModelTest.java
@@ -1,6 +1,5 @@
 package com.zalocoders.payoneer.payment_options.viewmodel;
 
-import androidx.lifecycle.Observer;
 import com.zalocoders.payoneer.RxImmediateSchedulerRule;
 import com.zalocoders.payoneer.data.models.payment_options.Applicable;
 import com.zalocoders.payoneer.payment_options.repository.PaymentOptionsRepository;
@@ -16,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -23,9 +23,6 @@ public class PaymentOptionViewModelTest {
 
     @Mock
     PaymentOptionsRepository paymentOptionsRepository;
-
-    @Mock
-    public Observer<List<Applicable>> observer;
 
     public PaymentOptionViewModel viewModel;
 
@@ -39,11 +36,21 @@ public class PaymentOptionViewModelTest {
 
     @Test
     public void gePaymentOptionList() {
+
         List<Applicable> applicable = new ArrayList<>();
         applicable.add(new Applicable());
         Mockito.when(paymentOptionsRepository.getPaymentOptions().map(it -> it.getNetworks().getApplicable())).thenReturn(Observable.just(applicable));
-        viewModel.gePaymentOptionList().observeForever(observer);
-        assert(viewModel.gePaymentOptionList().getValue() == null);
+
+        Observable<List<Applicable>> result =
+                paymentOptionsRepository.getPaymentOptions()
+                        .flatMap(response -> Observable.just(response.getNetworks().getApplicable()));
+
+
+        TestObserver<List<Applicable>> testObserver = new TestObserver<>();
+
+        result.subscribe(testObserver);
+        testObserver.assertComplete();
+        testObserver.assertNoErrors();
     }
 }
 
